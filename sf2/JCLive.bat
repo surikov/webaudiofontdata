e7e28a3cfcb8385008cedd294fc3740136d74d78
surikov <sss1024@gmail.com>
2017-04-10 13:37:45 +0300

    whole set

---
+++ sf2/JCLive.bat
@@ -0,0 +1,4 @@
+cd ../sound
+remjava -jar /home/sss/Desktop/Documents/jmuse-webaudiofont/jmuse-webaudiofont/sf2convert/executable/MuseScoreSF2.jar /home/sss/Desktop/Documents/GitHubWebAudioFont/webaudiofont/sf2/JCLive.sf2 22 ogg
+rempause
+
