0de1db3cff59cdf70b824b3d726a36d5f54bea35
surikov <sss1024@gmail.com>
2017-05-10 10:59:35 +0300

    SBLive uncompressed

---
+++ sf2/SBLive.bat
@@ -0,0 +1,5 @@
+cd ..\sound
+java -jar D:\projects\jmuse-webaudiofont\jmuse-webaudiofont\sf2convert\sources\netbeansproject\MuseScoreSF2\dist\MuseScoreSF2.jar ..\sf2\SBLive.sf2 128 wav
+java -jar D:\projects\jmuse-webaudiofont\jmuse-webaudiofont\sf2convert\sources\netbeansproject\MuseScoreSF2\dist\MuseScoreSF2.jar ..\sf2\SBLive.sf2 all wav
+pause
+
