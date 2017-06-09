87d7ed27cbaaa9e92629430db0a06eeb949f90c5
surikov <sss1024@gmail.com>
2017-06-09 13:04:25 +0300

    some waves

--- sf2/LesPaul.bat
+++ sf2/LesPaul.bat
@@ -1,4 +1,4 @@
 cd ..\sound
-java -jar D:\projects\jmuse-webaudiofont\jmuse-webaudiofont\sf2convert\sources\netbeansproject\MuseScoreSF2\dist\MuseScoreSF2.jar ..\sf2\LesPaul.sf2 all ogg
+java -jar D:\projects\jmuse-webaudiofont\jmuse-webaudiofont\sf2convert\sources\netbeansproject\MuseScoreSF2\dist\MuseScoreSF2.jar ..\sf2\LesPaul.sf2 all wav
 pause
 
